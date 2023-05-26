fd0f1737f53568087f1e98072fca22f4a6d2fb6e
Hector Dario SOL <hectordsol@gmail.com>
2023-05-26 15:32:46 -0300

    CORS fix 3

--- SW-api-microservices/database/src/server.js
+++ SW-api-microservices/database/src/server.js
@@ -6,10 +6,10 @@ const server = express();
 server.use(express.json());
 server.use(morgan("dev"));
 // server.use(cookieParser());
-const {MONGO_URI} = process.env;
-
+const {CORS} = process.env;
+console.log("cors: ", CORS);
 server.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', MONGO_URI); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', CORS); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
