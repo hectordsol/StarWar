a3f2e49f3b20123d613979ef3bf000f21d9ac6d0
Hector Dario SOL <hectordsol@gmail.com>
2023-05-26 17:23:21 -0300

    CORS fix 9

--- SW-api-microservices/Gateway/index.js
+++ SW-api-microservices/Gateway/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const server = express();
 const {createProxyMiddleware} = require("http-proxy-middleware");
+require('dotenv').config();
+const {CORS} = process.env;
+console.log("cors: ", CORS);
+server.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', CORS); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 server.use(morgan("dev"));
 server.use('/characters', createProxyMiddleware
