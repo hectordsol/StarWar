9b09364c8bde902dca482b1a804c9a142b75fb61
Hector Dario SOL <hectordsol@gmail.com>
2023-05-26 00:20:45 -0300

    fix changes 3 in models

--- SW-api-microservices/Planets/src/data/index.js
+++ SW-api-microservices/Planets/src/data/index.js
@@ -3,13 +3,14 @@ const planets = require('./planets.json');
 module.exports = {
     list: async () => {
         const response = await axios.get("http://database:8004/Planet");
-        return response.data;    },
+        return response.data;
+    },
     create: async(planet)=>{
-        const response = await axios.post("http://localhost:8004/Planet",planet);
+        const response = await axios.post("http://database:8004/Planet",planet);
         return response.data;
     },
     get: async(id)=>{
-        const response = await axios.get(`http://localhost:8004/Planet/${id}`);
+        const response = await axios.get(`http://database:8004/Planet/${id}`);
         return response.data;
     }
 }
